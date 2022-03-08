71f2f5ab2a0838e66f00f3f8cbadd0721bbd2735
to5ta <toby4mails@web.de>
2022-03-08 12:18:00 +0100

    simple ascii render script (using pillow and numpy)

---
+++ ascii_render.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from email.mime import image
+import shutil
+from PIL import Image
+import numpy as np
+
+size = shutil.get_terminal_size()
+print(f"Size: {  size}")
+GLYPH_SIZE_RATIO = 1/2.
+
+ar = [ "@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "." ]
+rmp  = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+img = Image.open("C://Users//tosta//Desktop//sortieren//noah_rbk.PNG")
+
+ratio = img.size[0] / img.size[1]
+
+x = size.columns
+y = img.size[1] / (img.size[0] / size.columns) * GLYPH_SIZE_RATIO
+
+img = img.resize([ int(x), int(y)])
+img = img.convert("L")
+
+imgdata = np.asarray(img)
+
+np.linalg.norm(imgdata)
+imgdata *= 255
+img.putdata(imgdata.flatten())
+# img.show()
+# print(f"Shape {imgdata.shape}")
+
+for y in range(0, imgdata.shape[0]):
+    for x in range(0, imgdata.shape[1]):
+        px = imgdata[y][x]
+        char = rmp[len(rmp) -1- px//len(rmp)]
+
+        print(char, end="")
+    print()
+        # print("h", end="")
